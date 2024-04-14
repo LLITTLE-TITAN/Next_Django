283f092df88f0837923e4ceed1b5b95c1e2d43db
HappySmile <153109864+devmad001@users.noreply.github.com>
2024-04-13 20:34:13 -0500

    a

--- app/(main)/edit/candidate/[id]/page.tsx
+++ app/(main)/edit/candidate/[id]/page.tsx
@@ -1,28 +1,27 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from "react";
 import { Checkbox, CheckboxChangeEvent } from "primereact/checkbox";
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { Dropdown } from 'primereact/dropdown';
-import { useFormState } from 'react-dom';
-import { create_candidate } from '@/app/lib/actions';
-import { gql, useQuery, useMutation } from '@apollo/client';
-import { useRouter } from 'next/navigation';
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Dropdown } from "primereact/dropdown";
+import { useFormState } from "react-dom";
+import { create_candidate } from "@/app/lib/actions";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { useRouter } from "next/navigation";
 
-import EditForm from './input'
+import EditForm from "./input";
 interface DropdownItem {
-    name: string;
-    code: string;
+  name: string;
+  code: string;
 }
 
 const FormLayoutDemo = ({ params }: { params: { id: string } }) => {
-    const router = useRouter();
-    const id = params.id;
+  const router = useRouter();
+  const id = params.id;
 
-    
-    const GET_CANDIDATE = gql`
+  const GET_CANDIDATE = gql`
     query candidateById {
         candidateById(id: "${id}") {
         id
@@ -32,19 +31,18 @@ const FormLayoutDemo = ({ params }: { params: { id: string } }) => {
         rateSalary
         }
     }
-    `; 
-    
-    
-    const initialState = { message: null, errors: {} };  
-      
-    const { loading, error, data:candidateData } = useQuery(GET_CANDIDATE);
-    if (loading) return <p>Loading...</p>; 
-     
-    return (
-        <>
-            <EditForm candidateData={candidateData}/>
-        </>
-    );
+    `;
+
+  const initialState = { message: null, errors: {} };
+
+  const { loading, error, data: candidateData } = useQuery(GET_CANDIDATE);
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <EditForm candidateData={candidateData} id={id} />
+    </>
+  );
 };
 
 export default FormLayoutDemo;
